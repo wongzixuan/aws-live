690151de2434dcb8c12cd1bf6ecc1c6f17aa1937
wongzixuan <113162858+wongzixuan@users.noreply.github.com>
2022-09-09 13:41:40 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.cmhfo8gyvwmt.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "wongzixuan-employee"
+customregion = "us-east-1"
